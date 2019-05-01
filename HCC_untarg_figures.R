e4c9de241e62f6141cd6e754d927b29b0322a6fe
JoeRothwell <joerothwell1@gmail.com>
2019-05-01 11:16:29 +0200

    Improvements to figures

--- HCC_untarg_figures.R
+++ HCC_untarg_figures.R
@@ -39,7 +39,7 @@ HCC.figs <- function(lev1 = F, type = c("correlation", "scores", "loadings")) {
   discs.impute <- na.aggregate(discs, FUN = median)
   
   # Run PCA
-  discpca <- prcomp(log(discs.impute), scale. = T)
+  discpca <- prcomp(log2(discs.impute), scale. = T)
   
   # Histogram of follow-up time
   hist(meta$Tfollowup, breaks = 20, col="dodgerblue", xlab = "Follow-up time (years)", 
@@ -77,7 +77,7 @@ HCC.figs <- function(lev1 = F, type = c("correlation", "scores", "loadings")) {
   if(type == "contributions") return(contr)
   
   # Remove labels and log transform
-  heatdata <- pcadata[, -(1:3)] %>% log
+  heatdata <- pcadata[, -(1:3)] %>% log2
   cormat <- cor(heatdata, use = "pairwise.complete.obs")
   
   if(type == "correlation") return(cormat)
@@ -87,22 +87,28 @@ HCC.figs <- function(lev1 = F, type = c("correlation", "scores", "loadings")) {
 # Plot figures for manuscript (supp 3A, 3B, 3C) ----
 
 dfscores <- HCC.figs(type = "scores")
-dfscores <- HCC.figs(type = "scores", lev1 = T)
+dfscores0 <- HCC.figs(type = "scores", lev1 = T)
 
 # Fig 3A: Single scores plot with cases and controls coloured only
-ggplot(dfscores, aes(x=PC1, y=PC2, colour=HCC)) + geom_point() + theme_bw() +
+library(ggplot2)
+library(scales)
+ggplot(dfscores0, aes(x=PC1, y=PC2, colour=HCC)) + geom_point() + theme_bw() +
   scale_shape_manual(values= c(17,19)) +
   xlab("Score on PC1") + ylab("Score on PC2") + geom_vline(xintercept=0, linetype = "dashed") +
-  geom_hline(yintercept=0, linetype = "dashed")
+  scale_y_continuous(labels = number_format(accuracy = 0.1)) +
+  geom_hline(yintercept=0, linetype = "dashed") +
+  theme(legend.position = "none")
 
 # Fig 3B: Follow-up time against PC1 with fitted lines
-ggplot(dfscores, aes(x=TF2, y=PC1, colour=HCC)) + geom_point() + theme_bw() + geom_smooth(method=lm) +
-  xlab("Follow-up time (years)") + ylab("Score on PC1") #+ ylim(-75,75)
+ggplot(dfscores0, aes(x=TF2, y=PC1, colour=HCC)) + geom_point() + theme_bw() + geom_smooth(method=lm) +
+  xlab("Follow-up time (years)") + ylab("Score on PC1")  +
+  theme(legend.position = "none")
 
 # Fig 3C: plot top 10 contributions for PC1, 2, 3. First prepare data then plot
 contr <- HCC.figs(type = "contributions")
+contr0 <- HCC.figs(type = "contributions", lev1 = T)
 
-ggplot(contr, aes(x = Compound, y=val, fill=PC)) + geom_bar(stat="identity") + coord_flip() + theme_bw() +
+ggplot(contr0, aes(x = Compound, y=val, fill=PC)) + geom_bar(stat="identity") + coord_flip() + theme_bw() +
   facet_grid(PC ~ .  , scales = "free_y") + ylab("Relative importance to PC") + 
   xlab("Components contributing most to PC") +
   theme(legend.position = "none")
